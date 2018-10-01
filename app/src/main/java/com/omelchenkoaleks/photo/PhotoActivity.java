b23ee9d30ef62f5e144acb97bdfb356427b7f690
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-01 17:01:28 +0300

    Intent. Используем системное приложение камера для фотографии...

--- app/src/main/java/com/omelchenkoaleks/photo/PhotoActivity.java
+++ app/src/main/java/com/omelchenkoaleks/photo/PhotoActivity.java
@@ -1,13 +1,81 @@
 package com.omelchenkoaleks.photo;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
 
 public class PhotoActivity extends Activity {
+    File directory;
+    private static final int PHOTO = 1;
+    private static final int REQUEST_CODE_PHOTO = 1;
+    private static final String TAG = "myLogs";
+    ImageView ivPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+
+        createDirectory();
+        ivPhoto = findViewById(R.id.ivPhoto);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent intent) {
+        if (requestCode == REQUEST_CODE_PHOTO) {
+            if (resultCode == RESULT_OK) {
+                if (intent == null) {
+                    Log.d(TAG, "Intent is null");
+                } else {
+                    Log.d(TAG, "Photo uri: " + intent.getData());
+                    Bundle bndl = intent.getExtras();
+                    if (bndl != null) {
+                        Object obj = intent.getExtras().get("data");
+                        if (obj instanceof Bitmap) {
+                            Bitmap bitmap = (Bitmap) obj;
+                            Log.d(TAG, "bitmap " + bitmap.getWidth() + " x "
+                                    + bitmap.getHeight());
+                            ivPhoto.setImageBitmap(bitmap);
+                        }
+                    }
+                }
+            } else if (resultCode == RESULT_CANCELED) {
+                Log.d(TAG, "Canceled");
+            }
+        }
+
+    }
+
+    public void onClickPhoto(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, generateFileUri(PHOTO));
+        startActivityForResult(intent, REQUEST_CODE_PHOTO);
+    }
+
+    private Uri generateFileUri(int type) {
+        File file = new File(directory.getPath() + "/" + "photo_"
+                + System.currentTimeMillis() + ".jpg");;
+        return Uri.fromFile(file);
+    }
+
+    // создаст папку для наших файлов и поместит файл в директорию
+    private void createDirectory() {
+        directory = new File(
+                Environment
+                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                "MyFolder");
+        if (!directory.exists()){
+            directory.mkdirs();
+        }
     }
 }
